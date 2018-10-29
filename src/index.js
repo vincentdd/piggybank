f7575a289a99188903b93ef34203d58aabe3ddb7
AUTOBVT-FASVEC2\Vincent <scar_dante@hotmail.com>
2018-10-29 22:26:52 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from "react-router-dom";
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
-
 ReactDOM.render( <App />, document.getElementById('root'));
 registerServiceWorker();
+
