d77c863b80929c5d4538bf307d6180c33f540247
AUTOBVT-FASVEC2\Vincent <scar_dante@hotmail.com>
2018-11-05 20:34:11 +0800

    no message

--- src/Components/Echarts.js
+++ src/Components/Echarts.js
@@ -8,30 +8,22 @@ import PropTypes from 'prop-types';
 export default class Chart extends Component {
     constructor(props) {
         super(props);
-        this.node = React.createRef();
+        this.chart = React.createRef();
+        this.initChart = this.initChart.bind(this);
     }
     componentDidMount() {
         debugger;
-        let doughuntChart = echarts.init(this.node);
-        doughuntChart.setOption({
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            xAxis: {
-                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        });
+        this.initChart();
+    }
+    initChart(){
+        const {data} = this.props;
+        let myChart = echarts.init(this.chart.current);
+        let options = this.setPieOption(data)
+        myChart.setOption(options)
     }
     render() {
         return(
-            <div ref={this.node} style={{width: "100%", height: "200px"}} />
+            <div ref={this.chart} style={{width: "100%", height: "200px"}} />
         )
     }
 }
--- src/Router/Home.js
+++ src/Router/Home.js
@@ -4,15 +4,11 @@ import BillList from '../Components/BillList';
 import Chart from '../Components/Echarts';
 import './Home.css';
 
-const data = [
-    {value: 1, name: "是"},
-    {value: 2, name: "否"}
-]
 const Home = () => {
     return (
         <div>
             <header><div className={"title"}>Piggy<span className={"orange-bgc"}>bank</span></div></header>
-            {/*<Chart />*/}
+            <Chart data={} />
             <NavLink to="/add/one">Add</NavLink>
             <NavLink to="/tags">View Tags</NavLink>
             <BillList />
