84eb138ad1c7dc2336d2f7ea03504a4736d1e356
AUTOBVT-FASVEC2\Vincent <scar_dante@hotmail.com>
2019-10-07 22:01:13 +0800

    using redux-saga to manage side effects

--- src/Components/ModalForm/BillForm.js
+++ src/Components/ModalForm/BillForm.js
@@ -1,38 +1,48 @@
-import React, { Component } from 'react';
-import { Form, Input, DatePicker, Select, Button, Radio, Modal } from 'antd';
+import React, {Component} from 'react';
+import moment from 'moment';
+import {Button, Modal, Form, Input, DatePicker} from 'antd';
 
-const BillForm = Form.create({ name: 'form_in_modal' })(
+const CollectionCreateForm = Form.create({name: 'bill_form'})(
     // eslint-disable-next-line
     class extends React.Component {
+        // componentDidMount(){
+        //     const { form } = this.props;
+        //     const { setFieldsValue } = form;
+        //     const bill = this.props.bill;
+        //     if(bill != undefined){
+        //         setFieldsValue({"context": bill.context});
+        //         setFieldsValue({"price": bill.price});
+        //     }
+        // }
         render() {
-            const { visible, onCancel, onCreate, form } = this.props;
-            const { getFieldDecorator } = form;
+            const {visible, onCancel, onCreate, form, bill} = this.props;
+            const {getFieldDecorator} = form;
             return (
                 <Modal
                     visible={visible}
-                    title="Create a new collection"
+                    title="编辑"
                     okText="Create"
                     onCancel={onCancel}
                     onOk={onCreate}
                 >
                     <Form layout="vertical">
-                        <Form.Item label="Title">
-                            {getFieldDecorator('title', {
-                                rules: [{ required: true, message: 'Please input the title of collection!' }],
-                            })(<Input />)}
+                        <Form.Item label="Context">
+                            {getFieldDecorator('context', {
+                                rules: [{required: true, message: 'Please input the context of collection!'}],
+                                initialValue: bill.context
+                            })(<Input/>)}
                         </Form.Item>
-                        <Form.Item label="Description">
-                            {getFieldDecorator('description')(<Input type="textarea" />)}
+                        <Form.Item label="Price">
+                            {getFieldDecorator('price', {
+                                rules: [{required: true, message: 'Please input the price of collection!'},{type:'string', pattern: /^[0-9]+(.[0-9]{2})?$/ , message: 'Price must be number.'}],
+                                initialValue: bill.price
+                            })(<Input type="textarea"/>)}
                         </Form.Item>
                         <Form.Item className="collection-create-form_last-form-item">
                             {getFieldDecorator('modifier', {
-                                initialValue: 'public',
-                            })(
-                                <Radio.Group>
-                                    <Radio value="public">Public</Radio>
-                                    <Radio value="private">Private</Radio>
-                                </Radio.Group>,
-                            )}
+                                rules: [{required: true, message: 'Please input the date of collection!'}],
+                                initialValue: moment(bill.createDate, "YYYY-MM-DD"),
+                            })(<DatePicker format="YYYY-MM-DD"/>)}
                         </Form.Item>
                     </Form>
                 </Modal>
@@ -41,4 +51,4 @@ const BillForm = Form.create({ name: 'form_in_modal' })(
     },
 );
 
-export default BillForm;
+export default CollectionCreateForm
--- src/services/bills.js
+++ src/services/bills.js
@@ -1,12 +1,20 @@
 import { request, config } from '../utils/index'
 
 const { api } = config;
-const { GET_ALL_BILLS } = api;
+const { GET_ALL_BILLS, POST_BILL } = api;
 
 export async function getAllBills (params) {
     return request({
         url: GET_ALL_BILLS,
         method: 'get',
+        data: params,
+    })
+}
+
+export async function postBill (params) {
+    return request({
+        url: POST_BILL,
+        method: 'post',
         data: params,
     })
 }
